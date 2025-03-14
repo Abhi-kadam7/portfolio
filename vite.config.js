56f2107340af3246c5a1cb7fe7f0aa6521dc5acd
Abhi-kadam7 <abhi77678842@gmail.com>
2025-03-15 00:29:26 +0530

    Updated portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { build, defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
-    build: {
-      outDir: "docs",
-    },
-  
+  build: {
+    outDir: 'dist',  // Change this from 'docs' to 'dist'
+  },
 });
